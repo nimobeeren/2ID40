b28fa61c2a08aef09e3f4e1dba059719926685ea
Nimo Beeren <nimobeeren@gmail.com>
2017-08-06 13:32:16 +0200

    Link to weekday page when card is clicked

--- js/overview.js
+++ js/overview.js
@@ -2,12 +2,38 @@ window.onload = function () {
     // Refresh data periodically
     setInterval(refresh, refreshTime);
     refresh();
+
+    Array.prototype.forEach.call(days, function (day) {
+        var content = document.querySelector('#' + day.toLowerCase() + ' .card__content');
+        var editButton = document.querySelector('#' + day.toLowerCase() + ' .edit-button');
+        var copyButton = document.querySelector('#' + day.toLowerCase() + ' .copy-button');
+
+        content.addEventListener('click', onEdit);
+        editButton.addEventListener('click', onEdit);
+        copyButton.addEventListener('click', onCopy);
+    })
 };
 
 function refreshUI() {
     setBackground(calcBackground());
     setLabelBackground(calcBackground());
     setTimelines();
+}
+
+function onEdit(event) {
+    var card = event.target;
+    while (!card.classList.contains('timelines__card')) {
+        try {
+            card = card.parentElement;
+        } catch (ex) {
+            throw new Error("Could not find period to edit");
+        }
+    }
+    window.location.href = 'weekday.html?day=' + card.id.charAt(0).toUpperCase() + card.id.slice(1);
+}
+
+function onCopy(event) {
+
 }
 
 function setTimelines() {
