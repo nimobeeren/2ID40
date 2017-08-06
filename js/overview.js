e46e5962994031f9f8c6a048292c338c5b4a69c9
Nimo Beeren <nimobeeren@gmail.com>
2017-08-06 13:55:32 +0200

    Implement copying of day programs

--- js/overview.js
+++ js/overview.js
@@ -1,3 +1,5 @@
+var copyProgram = null;
+
 window.onload = function () {
     // Refresh data periodically
     setInterval(refresh, refreshTime);
@@ -33,7 +35,65 @@ function onEdit(event) {
 }
 
 function onCopy(event) {
+    var card;
+    var buttons = document.getElementsByClassName('copy-button');
 
+    if (!copyProgram) {
+        // Change all other copy buttons to paste, and this one to cancel
+        Array.prototype.forEach.call(buttons, function (button) {
+            if (button === event.target) {
+                button.innerHTML = 'CANCEL';
+                button.classList.add('flat-button--red');
+            } else {
+                button.innerHTML = 'PASTE';
+            }
+        });
+
+        // Find program that needs to be copied
+        card = event.target;
+        while (!card.classList.contains('timelines__card')) {
+            try {
+                card = card.parentElement;
+            } catch (ex) {
+                throw new Error("Could not find period to copy");
+            }
+        }
+
+        // Store the program to be copied
+        copyProgram = weekProgram[card.id.charAt(0).toUpperCase() + card.id.slice(1)];
+    } else {
+        if (event.target.innerHTML === 'PASTE') {
+            // Find program that needs to be pasted on
+            card = event.target;
+            while (!card.classList.contains('timelines__card')) {
+                try {
+                    card = card.parentElement;
+                } catch (ex) {
+                    throw new Error("Could not find period to paste on");
+                }
+            }
+
+            // Display and save pasted program
+            weekProgram[card.id.charAt(0).toUpperCase() + card.id.slice(1)] = copyProgram;
+            refreshUI();
+            api.setWeekProgram(weekProgram);
+
+            // Restore copy buttons
+            Array.prototype.forEach.call(buttons, function (button) {
+                button.innerHTML = 'COPY';
+                button.classList.remove('flat-button--red');
+            });
+
+            copyProgram = null;
+        } else {
+            // Cancel the pasting
+            Array.prototype.forEach.call(buttons, function (button) {
+                button.innerHTML = 'COPY';
+                button.classList.remove('flat-button--red');
+            });
+            copyProgram = null;
+        }
+    }
 }
 
 function setTimelines() {
