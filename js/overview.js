af60f4cfb389833d03bb8d947bf34fe19c1eeef3
Nimo Beeren <nimobeeren@gmail.com>
2017-08-06 14:02:42 +0200

    Allow pasting by clicking on the content of a card

--- js/overview.js
+++ js/overview.js
@@ -10,7 +10,7 @@ window.onload = function () {
         var editButton = document.querySelector('#' + day.toLowerCase() + ' .edit-button');
         var copyButton = document.querySelector('#' + day.toLowerCase() + ' .copy-button');
 
-        content.addEventListener('click', onEdit);
+        content.addEventListener('click', onEditOrPaste);
         editButton.addEventListener('click', onEdit);
         copyButton.addEventListener('click', onCopy);
     })
@@ -20,6 +20,14 @@ function refreshUI() {
     setBackground(calcBackground());
     setLabelBackground(calcBackground());
     setTimelines();
+}
+
+function onEditOrPaste(event) {
+    if (copyProgram) {
+        onCopy(event);
+    } else {
+        onEdit(event);
+    }
 }
 
 function onEdit(event) {
@@ -35,8 +43,17 @@ function onEdit(event) {
 }
 
 function onCopy(event) {
-    var card;
     var buttons = document.getElementsByClassName('copy-button');
+
+    // Find program that needs to be copied or pasted on
+    var card = event.target;
+    while (!card.classList.contains('timelines__card')) {
+        try {
+            card = card.parentElement;
+        } catch (ex) {
+            throw new Error("Could not find period to copy");
+        }
+    }
 
     if (!copyProgram) {
         // Change all other copy buttons to paste, and this one to cancel
@@ -49,30 +66,11 @@ function onCopy(event) {
             }
         });
 
-        // Find program that needs to be copied
-        card = event.target;
-        while (!card.classList.contains('timelines__card')) {
-            try {
-                card = card.parentElement;
-            } catch (ex) {
-                throw new Error("Could not find period to copy");
-            }
-        }
-
         // Store the program to be copied
         copyProgram = weekProgram[card.id.charAt(0).toUpperCase() + card.id.slice(1)];
     } else {
-        if (event.target.innerHTML === 'PASTE') {
-            // Find program that needs to be pasted on
-            card = event.target;
-            while (!card.classList.contains('timelines__card')) {
-                try {
-                    card = card.parentElement;
-                } catch (ex) {
-                    throw new Error("Could not find period to paste on");
-                }
-            }
-
+        var copyButton = card.getElementsByClassName('copy-button')[0];
+        if (copyButton.innerHTML === 'PASTE') {
             // Display and save pasted program
             weekProgram[card.id.charAt(0).toUpperCase() + card.id.slice(1)] = copyProgram;
             refreshUI();
